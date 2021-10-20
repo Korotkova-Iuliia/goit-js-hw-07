45c9cf44789a125ea1ea50d094250ca0ed34e866
Korotkova-Iuliia <3589900@gmail.com>
2021-10-20 19:06:43 +0300

    task 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const imageContainer = document.querySelector('.gallery');
+imageContainer.addEventListener('click', onImageContainerClick);
+const imagesMarkup = galleryItems
+  .map(({ preview, description }) => {
+    return `
+     <div class="gallery__item">
+     <img
+     class="gallery__image"
+     src="${preview}" 
+     alt="${description}"
+    />
+    </div>
+    `;
+  })
+  .join('');
+imageContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+function onImageContainerClick(e) {}
 console.log(galleryItems);
