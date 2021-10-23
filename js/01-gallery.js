3f71cd6481bfd09e14fa4c416cf710343e5b7ef9
Korotkova-Iuliia <3589900@gmail.com>
2021-10-23 08:41:33 +0300

    task 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,6 +4,11 @@ import { galleryItems } from './gallery-items.js';
 const imageContainer = document.querySelector('.gallery');
 imageContainer.addEventListener('click', onImageContainerClick);
 // console.log(imageContainer);
+const keyDown = window.addEventListener('keydown', event);
+function event() {
+  console.log(keyDown);
+  console.log('adfsrer');
+}
 
 const imagesPreviewMarkup = galleryItems
   .map(({ preview, original, description }) => {
@@ -22,55 +27,101 @@ const imagesPreviewMarkup = galleryItems
   .join('');
 
 imageContainer.insertAdjacentHTML('beforeend', imagesPreviewMarkup);
-// console.log(imagesPreviewMarkup);
-
-// console.log(window.addEventListener('keydown', onImageContainerClick));
 
 function onImageContainerClick(e) {
-  e.preventDefault();
-  const isImageEl = e.target.classList.contains('gallery__image');
+  const url = getImageUrl(e.target);
+  console.log(url);
+
+  //   e.preventDefault();
+  //   const isImageEl = e.target.classList.contains('gallery__image');
 
   if (!isImageEl) {
     return;
   }
-  console.log('sgadaers');
-  console.log('Escape');
-  console.log(e.key === 'Escape');
-  instance.show();
-  const instance = basicLightbox.create(
-    `
-    <div class="modal">
-        <p>A custom modal that has been styled independently. 
-It's not part of basicLightbox, 
-but perfectly shows its flexibility.</p >
-        <input placeholder="Type something">
-        <a>Close</a>
-    </div>
-`,
-    {
-      onShow: (instance) => {
-        instance.element().querySelector('a').onclick = instance.close;
-      },
+  function onModalOpen(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+      return;
     }
-  );
-  //   const instance = basicLightbox.create(`
-  //     <div class="modal">
-  //         <p>
-  //             Your first lightbox with just a few lines of code.
-  //             Yes, it's really that simple.
-  //         </p>
-  //     </div>
-  // `);
-  //   instance.show(() => console.log('lightbox now visible'));
-  //   //   instance.close(() => console.log('lightbox not visible anymore'));
-  //   //   instance.show();
-  //   console.log(instance.show());
+    const url = getImageUrl(e.target);
+    instance = basicLightbox.create(
+      `<img src="${url}" width="800" height="600">`,
+      options
+    );
+    instance.show();
+  }
 }
+//   console.log('sgadaers');
+
+//   let instance = '';
+//   const options = {
+//     once: true,
+
+//     onShow: (instance) => {
+//       console.log('open');
+//       window.addEventListener('keydown', eventHandler);
+//       window.addEventListener('click', eventHandler);
+//       console.log('addEventListener');
+//     },
+
+//     onClose: (instance) => {
+//       console.log('close');
+//       window.removeEventListener('keydown', eventHandler);
+//       console.log('removeEventListener');
+//     },
+//   };
+//   function eventHandler(e) {
+//     if (e.key === 'Escape') {
+//       console.log('key', e.key);
+//       instance.close();
+//       return;
+//     }
+// }
+
+// вариант с модалкой  - 1 рабочий, все слушатели снимаются (по идее)
+//   function onModalOpen(e) {
+// e.preventDefault();
+//     if (e.target.nodeName !== 'IMG') {
+//       return;
+//     }
+//     const url = getImageUrl(e.target);
+//     instance = basicLightbox.create(
+//       `<img src="${url}" width="800" height="600">`,
+//       options
+//     );
+// вешаем слушателей на клик и на esc при октрытии модалки
+// instance.show();
+//   }
+// }
+
+// ваиант с модалкой
+//   const instance = basicLightbox.create(
+//     `
+//       <div class="modal">
+//            <p>
+//               Your first lightbox with just a few lines of code.
+//               Yes, it's really that simple.
+//           </p>
+//           <a>Close</a>
+//       </div>
+//   `,
+//     {
+//       onShow: (instance) => {
+//         instance.element().querySelector('a').onclick = instance.close;
+//       },
+//     }
+//   );
+//   instance.show();
+
+//   instance.show(() => console.log('lightbox now visible'));
+//   //   instance.close(() => console.log('lightbox not visible anymore'));
+//   //   instance.show();
+//   console.log(instance.show());
 
 // instance.show();
 
 // основа 2
-// let instance = 'onImageContainerClick';
+// let instance = '';
 // const options = {
 //   once: true,
 //   onShow: (instance) => {
