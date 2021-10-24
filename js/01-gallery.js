ad65865372951873f5bb0e3d4e640dcc9feb38bd
Korotkova-Iuliia <3589900@gmail.com>
2021-10-24 08:20:34 +0300

    task 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,17 +4,12 @@ import { galleryItems } from './gallery-items.js';
 const imageContainer = document.querySelector('.gallery');
 imageContainer.addEventListener('click', onImageContainerClick);
 // console.log(imageContainer);
-const keyDown = window.addEventListener('keydown', event);
-function event() {
-  console.log(keyDown);
-  console.log('adfsrer');
-}
 
 const imagesPreviewMarkup = galleryItems
   .map(({ preview, original, description }) => {
     return `
     <div class="gallery__item">
-  <a class="gallery__link" href="${original}">
+  <a id="aaa" class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -28,135 +23,61 @@ const imagesPreviewMarkup = galleryItems
 
 imageContainer.insertAdjacentHTML('beforeend', imagesPreviewMarkup);
 
+let instance = '';
+
+const options = {
+  once: true,
+  onShow: (instance) => {
+    console.log('open');
+    window.addEventListener('keydown', eventHandler);
+
+    console.log('addEventListener');
+  },
+  onClose: (instance) => {
+    console.log('close');
+    window.removeEventListener('keydown', eventHandler);
+    console.log('removeEventListener');
+  },
+};
+instance = basicLightbox.create(
+  `
+    <div class="modal">
+           instance.element().querySelector('img').onclick = instance.close;
+ <a>Close</a>
+    </div>
+`,
+  options
+);
 function onImageContainerClick(e) {
-  const url = getImageUrl(e.target);
-  console.log(url);
-
-  //   e.preventDefault();
-  //   const isImageEl = e.target.classList.contains('gallery__image');
-
+  e.preventDefault();
+  const isImageEl = e.target.classList.contains('gallery__image');
   if (!isImageEl) {
     return;
   }
-  function onModalOpen(e) {
-    e.preventDefault();
-    if (e.target.nodeName !== 'IMG') {
-      return;
+  const altImageSmall = e.target.getAttribute('alt');
+  for (let i = 0; i < galleryItems.length; i++) {
+    const galleryItem = galleryItems[i];
+    const srcGalleryItem = galleryItem.original;
+    const altGalleryItem = galleryItem.description;
+    if (altGalleryItem === altImageSmall) {
+      const instance = basicLightbox.create(`
+                <img src="${srcGalleryItem}" width="800" height="600">
+
+        `);
     }
-    const url = getImageUrl(e.target);
-    instance = basicLightbox.create(
-      `<img src="${url}" width="800" height="600">`,
-      options
-    );
-    instance.show();
+  }
+  instance.show();
+}
+
+function eventHandler(e) {
+  if (e.key === 'Escape') {
+    console.log('key', e.key);
+    instance.close();
+    return;
   }
 }
-//   console.log('sgadaers');
 
-//   let instance = '';
-//   const options = {
-//     once: true,
-
-//     onShow: (instance) => {
-//       console.log('open');
-//       window.addEventListener('keydown', eventHandler);
-//       window.addEventListener('click', eventHandler);
-//       console.log('addEventListener');
-//     },
-
-//     onClose: (instance) => {
-//       console.log('close');
-//       window.removeEventListener('keydown', eventHandler);
-//       console.log('removeEventListener');
-//     },
-//   };
-//   function eventHandler(e) {
-//     if (e.key === 'Escape') {
-//       console.log('key', e.key);
-//       instance.close();
-//       return;
-//     }
-// }
-
-// вариант с модалкой  - 1 рабочий, все слушатели снимаются (по идее)
-//   function onModalOpen(e) {
-// e.preventDefault();
-//     if (e.target.nodeName !== 'IMG') {
-//       return;
-//     }
-//     const url = getImageUrl(e.target);
-//     instance = basicLightbox.create(
-//       `<img src="${url}" width="800" height="600">`,
-//       options
-//     );
-// вешаем слушателей на клик и на esc при октрытии модалки
-// instance.show();
-//   }
-// }
-
-// ваиант с модалкой
-//   const instance = basicLightbox.create(
-//     `
-//       <div class="modal">
-//            <p>
-//               Your first lightbox with just a few lines of code.
-//               Yes, it's really that simple.
-//           </p>
-//           <a>Close</a>
-//       </div>
-//   `,
-//     {
-//       onShow: (instance) => {
-//         instance.element().querySelector('a').onclick = instance.close;
-//       },
-//     }
-//   );
-//   instance.show();
-
-//   instance.show(() => console.log('lightbox now visible'));
-//   //   instance.close(() => console.log('lightbox not visible anymore'));
-//   //   instance.show();
-//   console.log(instance.show());
-
-// instance.show();
-
-// основа 2
-// let instance = '';
-// const options = {
-//   once: true,
-//   onShow: (instance) => {
-//     console.log('open');
-//     window.addEventListener('keydown', onImageContainerClick);
-//     console.log('addEventListener');
-//   },
-
-//   onClose: (instance) => {
-//     console.log('close');
-//     window.removeEventListener('keydown', onImageContainerClick);
-//     console.log('removeEventListener');
-//   },
-// };
-// function onImageContainerClick(e) {
-//   if (e.key === 'Escape') {
-//     console.log('key', e.key);
-//     instance.close();
-//     return;
-//   }
-// }
-// // вариант с модалкой  - 1 рабочий, все слушатели снимаются (по идее)
-// function onModalOpen(e) {
-//   e.preventDefault();
-//   if (e.target.nodeName !== 'IMG') {
-//     return;
-//   }
-//   const url = getImageUrl(e.target);
-//   instance = basicLightbox.create(
-//     `<img src="${url}" width="800" height="600">`,
-//     options
-//   );
-//   // вешаем слушателей на клик и на esc при октрытии модалки
-//   instance.show();
-// }
+/
 
 // вариант 1
 // const imageContainer = document.querySelector('.gallery');
